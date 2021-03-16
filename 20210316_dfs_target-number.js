afa89e1c89a68e552d183f9adc07f15a04ff9cf0
SSG\q901vx <car10yd2@daum.net>
2021-03-16 10:49:47 +0900

    Refactoring Target Number Issue

--- 20210316_dfs_target-number.js
+++ 20210316_dfs_target-number.js
@@ -1,15 +1,12 @@
 // Quiz LinK: https://programmers.co.kr/learn/courses/30/lessons/43165
 function solution(numbers, target) {
-  let answer = 0;
   const dfs = (x, value) => {
-    if (x < numbers.length) {
-      dfs(x + 1, value + numbers[x]);
-      dfs(x + 1, value - numbers[x]);
-    } else if (value === target) answer += 1;
+    if (x < numbers.length) return dfs(x + 1, value + numbers[x]) + dfs(x + 1, value - numbers[x]);
+    else if (value === target) return 1;
+    else return 0;
   };
-  dfs(0, 0);
 
-  return answer;
+  return dfs(0, 0);
 }
 
 console.log(solution([1, 1, 1, 1, 1], 3));
