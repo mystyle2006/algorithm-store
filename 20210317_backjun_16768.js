1e2dc6780c35ce821fba255548d78ba3d1512da5
SSG\q901vx <car10yd2@daum.net>
2021-03-18 14:10:38 +0900

    Resolve Backjun 16768 issue (hard level)

--- 20210317_backjun_16768.js
+++ 20210317_backjun_16768.js
@@ -1,74 +1,87 @@
-// link : https://www.acmicpc.net/problem/17413
+// link : https://www.acmicpc.net/problem/16768
 
-function solution(M, N, cabbages) {
-  const ground = Array.from(Array(M), () => Array.from(Array(N), () => 0));
-  cabbages.forEach(([from, to]) => {
-    ground[from][to] = 1;
-  });
+function solution(N, S, board) {
+  const COL = 10;
+  let checked = Array.from(Array(N), () => Array.from(Array(COL), () => 0));
 
-  const checked = Array.from(Array(M), () => Array.from(Array(N), () => false));
-  let cnt = 0;
-
-  function dfs(i, j) {
-    if (i < 0 || j < 0 || i >= M || j >= N || checked[i][j] || ground[i][j] === undefined || ground[i][j] === 0) return;
+  function remove(i, j) {
+    if (checked[i][j] === 0 || checked[i][j] === undefined) return;
     else {
-      checked[i][j] = true;
-      dfs(i + 1, j);
-      dfs(i - 1, j);
-      dfs(i, j + 1);
-      dfs(i, j - 1);
+      board[i][j] = -1;
+      checked[i][j] = 0;
+
+      if (i + 1 < N) remove(i + 1, j);
+      if (i - 1 >= 0) remove(i - 1, j);
+      if (j + 1 < COL) remove(i, j + 1);
+      if (j - 1 >= 0) remove(i, j - 1);
     }
   }
 
-  for(let i = 0; i < M; i += 1) {
-    for (let j = 0; j < N; j += 1) {
-      if (!checked[i][j] && ground[i][j] === 1) {
-        dfs(i, j);
-        cnt += 1;
+  function dfs(i, j, flag) {
+    if (checked[i][j] || board[i][j] === undefined || board[i][j] !== flag) return 0;
+    else {
+      checked[i][j] = 1;
+
+      const right =  (i + 1 < N) ? dfs(i + 1, j, flag) : 0;
+      const left =  (i - 1 >= 0) ? dfs(i - 1, j, flag) : 0;
+      const bottom =  (j + 1 < COL) ? dfs(i, j + 1, flag) : 0;
+      const top = (j - 1 >= 0) ? dfs(i, j - 1, flag) : 0;
+      return 1 + right + left + bottom + top;
+    }
+  }
+
+  function down(current, col) {
+    if (current < 0) return;
+    if (board[current][col] !== -1) down(current - 1, col);
+    else {
+      let valueOnTop = board[current - 1][col];
+      if (valueOnTop === undefined || valueOnTop === 0) {
+        board[current][col] = 0;
+        return;
+      }
+
+      if (valueOnTop === -1) {
+        down(current - 1, col);
+        valueOnTop = board[current - 1][col];
+      }
+
+      board[current][col] = valueOnTop;
+      board[current - 1][col] = -1;
+      down(current - 1, col);
+    }
+  }
+
+  while(true) {
+    let exist = false;
+
+    for(let i = N - 1; i >= 0; i -= 1) {
+      for (let j = 0; j < COL; j++) {
+        if (![0, -1].includes(board[i][j]) && !checked[i][j]) {
+          const cnt = dfs(i, j, board[i][j]);
+          if (cnt < S) continue;
+          remove(i, j);
+          exist = true;
+        }
       }
     }
+
+    if (!exist) {
+      break;
+    }
+
+    for (let k = 0; k < COL; k += 1) {
+      down(N - 1, k);
+    }
   }
 
-  return cnt;
+  return board;
 };
 
-console.log(solution(10, 8, [
-  [0, 0],
-  [1, 0],
-  [1, 1],
-  [4, 2],
-  [4, 3],
-  [4, 5],
-  [2, 4],
-  [3, 4],
-  [7, 4],
-  [8, 4],
-  [9, 4],
-  [7, 5],
-  [8, 5],
-  [9, 5],
-  [7, 6],
-  [8, 6],
-  [9, 6],
+console.log(solution(6, 3, [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,3,0,0],
+  [0,0,5,4,0,0,0,3,0,0],
+  [1,0,5,4,5,0,2,2,3,0],
+  [2,2,1,1,1,2,2,2,2,0],
+  [1,1,1,1,1,1,1,2,2,3],
 ]));
-
-console.log(solution(10, 10, [
-  [5, 5],
-]));
-
-console.log(solution(5, 3, [
-  [0, 2],
-  [1, 2],
-  [2, 2],
-  [3, 2],
-  [4, 2],
-  [4, 0],
-]));
-
-
-0000000000
-0000000000
-0000000000
-0000000000
-0000000000
-1000500000
