eb82d4965eeedcf222561cbac24bacfdcbc6933a
SSG\q901vx <car10yd2@daum.net>
2021-03-19 15:09:26 +0900

    Finally resolve modusign 2 issue

--- 20210316_modusign_2.js
+++ 20210316_modusign_2.js
@@ -3,6 +3,16 @@
 // 출력: 2
 
 function solution(list, k) {
+  let answer = 0;
+  for (let i = 0; i < list.length; i += 1) {
+    let sum = 0;
+    for(let j = i; j < list.length; j += 1) {
+      sum += list[j];
+      if (sum === k) answer += 1;
+    }
+  }
+
+  return answer;
 }
 
 console.log(solution([1, 1, 1, 1], 3));
